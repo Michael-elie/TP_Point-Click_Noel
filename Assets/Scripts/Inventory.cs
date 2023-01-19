e0f2a2bb5abe04168e55e3352f33082772d2f26f
Michael-elie <120110492+Michael-elie@users.noreply.github.com>
2023-01-19 01:25:59 +0100

    Animation porte + posterUnity

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -15,8 +15,8 @@ public class Inventory : MonoBehaviour
    private Indices _indices;
    public TextMeshProUGUI IndiceText; 
   public float IndiceNumber = 7f ;
- 
-
+  [SerializeField] private  Button BookButton;
+  public  Image Booksprite;
  
   
   
@@ -24,12 +24,17 @@ public class Inventory : MonoBehaviour
    public void Start()
    {
        _indices = FindObjectOfType<Indices>();
-
+       BookButton.interactable = false;
    }
 
    public void Update()
    {
-      
+       if (  Booksprite.GetComponent<Image>().color == Color.white)
+       {
+           BookButton.interactable = true;
+       }
+
+     
    }
 
    public void IndiceUpdate()
